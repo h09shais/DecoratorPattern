7e117927c871e4a75d3e9a1fb43e263021b2f4b3
Shanawaz Islam <sis@nisportal.com>
2018-10-17 17:08:28 +0200

    Added test project

---
+++ DecoratorPattern.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DecoratorPattern.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DecoratorPattern.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1a4ad97b-e404-4dea-850b-189f9194cdd2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DecoratorPattern.Test/UnitTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace DecoratorPattern.Test
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void CoffeeWithMilkCostlyThenSimpleCoffee()
+        {
+            var coffee = new Mock<ICoffee>();
+            var simpleCoffee = new SimpleCoffee();
+            var coffeeWithMilk = new WithMilk(simpleCoffee);
+            Assert.IsTrue(coffeeWithMilk.Cost > simpleCoffee.Cost);
+        }
+    }
+}
